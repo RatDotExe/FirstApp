b984e7b47bf1e2a4565caf22ee13d4e29f16c9c7
Zachary <rat@ratdotexe.com>
2013-03-23 17:56:18 -0400

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Zachary on 3/23/13.
+//  Copyright (c) 2013 Zachary. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
